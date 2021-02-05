35f11f1ee79216d303de52bcfc16c4e28adc2339
Daniel Furman <dryanfurman@gmail.com>
2021-02-04 16:42:27 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ setup(
     author_email="dryanfurman@gmail.com",
     description=("Class for MCMC serach in Python"),
     license=license,
+    long_description=readme,
+    long_description_content_type='text/markdown'
     classifiers=[
         "Topic :: MCMC for predictive modeling",
         "License :: MIT",
